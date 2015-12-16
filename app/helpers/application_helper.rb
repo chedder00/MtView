c5b6bea593db7e7b5b705eff3b56e3b70d4fad0f
Chad Greene <chedder00@gmail.com>
2015-12-16 10:13:27 -0800

    Finished helper to clean up page titles and standardize them

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Establishes full page title for each page rendered
+  def full_title(page_title = '')
+    site_title = "Mountain View Medicals"
+    if page_title.empty?
+      site_title
+    else
+      page_title + " | " + site_title
+    end
+  end
 end
