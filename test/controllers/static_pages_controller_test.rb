beeef46b9f213bb5325493fef716bbb7eb1fcb32
Chad Greene <chedder00@gmail.com>
2015-12-16 09:26:00 -0800

    Finished Basic static pages skeleton framework and updated Gems for project

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Mountain View Medicals"
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}" 
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | #{@base_title}" 
+  end
+
+end
