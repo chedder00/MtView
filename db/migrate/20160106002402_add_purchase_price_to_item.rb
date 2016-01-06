13304ba176f9d79895e4d6b860fc3539bd226811
Chad Greene <chedder00@gmail.com>
2016-01-05 17:06:03 -0800

    Fixed database column issue for items

--- db/migrate/20160106002402_add_purchase_price_to_item.rb
+++ db/migrate/20160106002402_add_purchase_price_to_item.rb
@@ -1,5 +1,5 @@
 class AddPurchasePriceToItem < ActiveRecord::Migration
   def change
-    add_column :items, :purchase_price_cents, :monetize, default: 0
+    add_column :items, :purchase_price_cents, :integer, default: 0
   end
 end
