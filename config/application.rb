87b743d418c8955b98269b6db5819d5941857289
Chad Greene <chedder00@gmail.com>
2016-01-21 21:58:38 -0800

    New Wicked PDF version

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module MtView
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.compile = true
     config.assets.precompile += ['custom.css', 'pdf.css']
   end
 end
