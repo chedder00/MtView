935d7656427d1025fd684dd0848521ca956448f3
Chad Greene <chedder00@gmail.com>
2016-01-23 19:56:05 -0800

    Finished PDF generation

--- app/grids/full_order.rb
+++ app/grids/full_order.rb
@@ -12,24 +12,26 @@ class FullOrder < BaseGrid
 
   filter(:submitted, :xboolean)
 
+  filter(:completed, :xboolean)
+
   column(:order_number, order: 'order.id', html: true) do |ord|
     link_to ord.id, user_order_path(user_id: ord.user.id, id: ord.id)
   end
   
-  column(:inventory_items, order: proc {|s|
-      s.joins(:items).order('items.inventory_item_id')}) do |order|
+  column(:inventory_items, order: 'orders.id') do |order|
     render 'shared/paginate_list', object: order.inventory_items,
                               locals: { class_name: "index in-table",
                                         in_table: true }
   end
 
-  column(:subtotal, order: proc {|s| 
-    s.joins(:items).order('items.inventory_item_id')}) do |order|
+  column(:subtotal) do |order|
     render 'shared/paginate_list', object: order.items,
                                    locals: { class_name: "index in-table",
                                              in_table: true }
   end
 
-  column(:total)
+  column(:total) do |order|
+    humanized_money_with_symbol(order.total)
+  end
 
 end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,3 +1,10 @@
 module ReportsHelper
-
+  def pdf_link(params)
+    p = params["#{@grid.class.to_s.underscore}"]
+    link = "#{@grid.class.to_s.underscore}.pdf?utf8="
+    p.each do |k, v|
+      link +="&#{@grid.class.to_s.underscore}%5B#{k}%5D=#{v}"
+    end
+    link
+  end
 end
